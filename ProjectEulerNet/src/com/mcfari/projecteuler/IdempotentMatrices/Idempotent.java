f6be6d15f2efe84defe630931f158ffe30657e8d
imackintouch <imackintouch@gmail.com>
2016-11-06 14:43:11 -0500

    Renamed ProjectEulerJava directory to ProjectEulerNet

--- ProjectEulerNet/src/com/mcfari/projecteuler/IdempotentMatrices/Idempotent.java
+++ ProjectEulerNet/src/com/mcfari/projecteuler/IdempotentMatrices/Idempotent.java
@@ -19,44 +19,37 @@ public class Idempotent {
 			
 		int min=-n;
 		int max=n;
-		//double countIndempotent=0;
 		int countIndempotent=0;
 		//double iteration=0;
+		//double countIndempotent=0;
+		
+		//Let's solve via equations based on matrix multiplication rules instead.
+	
 		for (int a=min; a<=max; a++ ) {
-			//A.setElement(0, 0, a);	//B.setElement(0, 0, a);
 			for (int b=min; b<=max; b++ ) {
-				//A.setElement(0, 1, b);	//B.setElement(0, 1, b);
 				for (int c=min; c<=max; c++ ) {
-					//A.setElement(0, 2, c);	//B.setElement(0, 2, c);
 					for (int d=min; d<=max; d++ ) {
-						//A.setElement(1, 0, d);	//B.setElement(1,0, d);
 						for (int e=min; e<=max; e++ ) {
-							//A.setElement(1, 1, e);	//B.setElement(1, 1, e);
 							for (int f=min; f<=max; f++ ) {
-								//A.setElement(1, 2, f);	//B.setElement(1, 2, f);
 								for (int g=min; g<=max; g++ ) {
-									//A.setElement(2, 0, g);	//B.setElement(2, 0, g);
+									if (a*a + b*d + c*g	!= a) //solution for A dot A - [0,0]
+										continue;
+									if (d*a + e*d + f*g	!= d) //solution for A dot A - [1,0]
+										continue;
 									for (int h=min; h<=max; h++ ) {
-										//A.setElement(2, 1, h);	//B.setElement(2, 1, h);
+										if (a*b + b*e + c*h != b) //solution for A dot A - [0,1]
+											continue;
+										if (d*b + e*e + f*h != e) //solution for A dot A - [1,1]
+											continue;
 										for (int i=min; i<=max; i++ ) {
-											//A.setElement(2, 2, i);	//B.setElement(2, 2, i);
+											
 											//iteration++;
 											//System.out.printf("%d %d %d %d %d %d %d %d %d \tIteration:%6d\n",a,b,c,d,e,f,g,h,i,iteration);
-											//Let's solve via equations based on matrix multiplication rules instead.
-											if (a*a + b*d + c*g	!= a) //solution for A dot A - [0,0]
-												continue;
-											if (a*b + b*e + c*h != b) //solution for A dot A - [0,1]
-												continue;
+																						
 											if (a*c + b*f + c*i != c) //solution for A dot A - [0,2]
-												continue;
-											
-											if (d*a + e*d + f*g	!= d) //solution for A dot A - [1,0]
-												continue;
-											if (d*b + e*e + f*h != e) //solution for A dot A - [1,1]
-												continue;
+												continue;		
 											if (d*c + e*f + f*i != f) //solution for A dot A - [1,2]
 												continue;
-											
 											if (g*a + h*d + i*g	!= g) //solution for A dot A - [2,0]
 												continue;
 											if (g*b + h*e + i*h != h) //solution for A dot A - [2,1]
@@ -68,26 +61,12 @@ public class Idempotent {
 											//System.out.printf("\nFound an indempotent matrix at iteration:%f \n",Math.floor(iteration));
 											System.out.println("\nFound an indempotent matrix. (See below):");
 											
-											A.setElement(0, 0, a);	
-											A.setElement(0, 1, b);	
-											A.setElement(0, 2, c);	
-											A.setElement(1, 0, d);	
-											A.setElement(1, 1, e);	
-											A.setElement(1, 2, f);	
-											A.setElement(2, 0, g);	
-											A.setElement(2, 1, h);	
-											A.setElement(2, 2, i);	
+											A.setElement(0, 0, a);	A.setElement(0, 1, b);	A.setElement(0, 2, c);	
+											A.setElement(1, 0, d);	A.setElement(1, 1, e);	A.setElement(1, 2, f);	
+											A.setElement(2, 0, g);	A.setElement(2, 1, h);	A.setElement(2, 2, i);	
 											
 											A.display();
 											System.out.println();
-											
-											/*if (Matrix.equals(A,Matrix.multiply(A, B))) {
-												countIndempotent++;
-												System.out.printf("\nFound an idempotent matrix at iteration:%f \n",iteration);
-												A.display();
-												System.out.println();
-											}*/
-											
 										}
 									}
 								}
