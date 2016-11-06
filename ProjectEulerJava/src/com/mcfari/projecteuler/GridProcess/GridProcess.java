dddcdb65325367c4bbce74da00f3e94cc4a617fd
imackintouch <imackintouch@gmail.com>
2016-11-06 13:00:01 -0500

    Removed some of the commented out code from GridProcess.java for the sake of readability.

--- ProjectEulerJava/src/com/mcfari/projecteuler/GridProcess/GridProcess.java
+++ ProjectEulerJava/src/com/mcfari/projecteuler/GridProcess/GridProcess.java
@@ -16,45 +16,23 @@ public class GridProcess {
 		String gridInputFilePath="C:\\users\\imack\\Documents\\euler_project_20x20_grid.txt";
 		File gridInputFile = new File(gridInputFilePath);
 				
-				
 		//Now populate grid from a file.
 		eulerGrid.populateGridFromFile(gridInputFile);
-		//eulerGrid.displayGrid();
 		
-		//System.out.println("End of grid input display");
-		
-		int x=6,y=8;
 		int groupSize=4;
-		//eulerGrid.getNumGroups(4, 3, 4);
-		//eulerGrid.getNumGroups(7, 6, 4);
-		//eulerGrid.getNumGroups(0, 0, 4);
-		eulerGrid.getNumGroups(6, 8, groupSize);
-		
-		//System.exit(0);
-		
 		LinkedList<Integer> productList = new LinkedList<Integer>();
 		
-		System.out.println("Pivot Co-ordinate (x,y): (" + x + ", " + y + ")");
+		
 		//For each of the list of numbers returned from the methods below, add the last element which will be
 		//a product.
-		//height=5;
-		//width=5;
 		for (int i=0; i<height; i++) {
 			for (int j=0; j<width; j++) {
-				/*productList.add(eulerGrid.getNorthGroup(i, j, groupSize).getLast());
-				productList.add(eulerGrid.getSouthGroup(i, j, groupSize).getLast());
-				productList.add(eulerGrid.getWestGroup(i, j, groupSize).getLast());
-				productList.add(eulerGrid.getEastGroup(i, j, groupSize).getLast());
-				productList.add(eulerGrid.getNorthWestGroup(i, j, groupSize).getLast());
-				productList.add(eulerGrid.getNorthEastGroup(i, j, groupSize).getLast());
-				productList.add(eulerGrid.getSouthWestGroup(i, j, groupSize).getLast());
-				productList.add(eulerGrid.getSouthEastGroup(i, j, groupSize).getLast());*/
 				processGridElement(i, j, groupSize, productList, eulerGrid);
 				System.out.println("List of Product values derived as of "+i+"," + j + "is:" + productList);
 			}
 		}
+		
 		//Run through productList to get the maximum value
-					
 		System.out.printf("The maximum %d group product value is:%d",groupSize, getMax(productList));
 		
 	}
@@ -72,7 +50,7 @@ public class GridProcess {
 	
 	public static void processGridElement(int x, int y, int groupSize, 
 				LinkedList<Integer> productList, Grid eulerGrid) {
-		//LinkedList<Integer> productList = new LinkedList<Integer>();
+		
 		productList.add(eulerGrid.getNorthGroup(x, y, groupSize).getLast());
 		productList.add(eulerGrid.getSouthGroup(x, y, groupSize).getLast());
 		productList.add(eulerGrid.getWestGroup(x, y, groupSize).getLast());
@@ -82,6 +60,5 @@ public class GridProcess {
 		productList.add(eulerGrid.getSouthWestGroup(x, y, groupSize).getLast());
 		productList.add(eulerGrid.getSouthEastGroup(x, y, groupSize).getLast());
 		
-		//return productList;
 	}
 }
